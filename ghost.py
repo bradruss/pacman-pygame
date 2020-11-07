c617670e0f81d7f8f94980f3dcc16bcfde8ac845
Bradley Russavage <brussava@uvm.edu>
2020-11-07 15:52:55 -0500

    Implement pacman class and ghost hotfix

--- ghost.py
+++ ghost.py
@@ -1,28 +1,23 @@
 import pygame
 
-class Ghost ():
-    window_height = 600
-    window_width = 1200
-    pygame.init()
-    disp = pygame.display.set_mode((window_width,window_height))   
-    images_original = [pygame.image.load ("strongGhost.png").convert_alpha(),
-              pygame.image.load ("weakGhost.png").convert_alpha()]
+class Ghost():
+    images_original = [pygame.image.load ("strongGhost.png"),
+              pygame.image.load ("weakGhost.png")]
     images = [pygame.transform.rotozoom(images_original[0],0,2),
              pygame.transform.rotozoom(images_original[1],0,0.15)]
+    ghosts = []
         
     def __init__(self):
-        disp = pygame.display
         self.surface = Ghost.images [0]
         self.weak = False
         self.time = 0
         self.course = 10
-        self.rect = self.surface.get_rect();
+        self.rect = self.surface.get_rect()
         self.rect.left = 3
-        self.rect.top = 100
+        #self.rect.top = 100
 
-
-    def addGhost(self, ghost):
-        ghosts.append(Ghost)
+    # def addGhost(self, ghost):
+    #     self.ghosts.append(ghost)
 
     def notVulnerable(self):
         self.surface = Ghost.images [0]
@@ -33,14 +28,14 @@ class Ghost ():
         self.weak = True
         self.time = 10
 
-    def moveGhosts(self, pacman):
-        ver = pacman.rect.left - self.rect.left
-        hor = pacman.rect.top - self.rect.top
-        if ver > 0 and level_one.check_valid(self.rect.left,self.rect.top):
-            self.rect.left -= 1
-        elif ver < 0 and level_one.check_valid(self.rect.left,self.rect.top):
-            self.rect.left +=1
-        elif hor > 0 and level_one.check_valid(self.rect.left,self.rect.top):
-            self.rect.top -=1
+    # def moveGhosts(self, pacman):
+    #     ver = pacman.rect.left - self.rect.left
+    #     hor = pacman.rect.top - self.rect.top
+    #     if ver > 0 and level_one.check_valid(self.rect.left,self.rect.top):
+    #         self.rect.left -= 1
+    #     elif ver < 0 and level_one.check_valid(self.rect.left,self.rect.top):
+    #         self.rect.left +=1
+    #     elif hor > 0 and level_one.check_valid(self.rect.left,self.rect.top):
+    #         self.rect.top -=1
         
             
