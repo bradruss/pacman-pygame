0bb896be53d463ef401b1714a01b0447fa532329
brandonleetheasian <bjlee@uvm.edu>
2020-11-27 15:02:57 -0500

    algorithms should be good rn

--- ghost.py
+++ ghost.py
@@ -1,15 +1,38 @@
 import pygame
+import random
+from pacman import Pacman
+import level
+import copy
+import time
+
 
 class Ghost():
     weakGhost = pygame.image.load("weakGhost.png")
     images = [pygame.image.load("redGhost.png"), pygame.image.load("blueGhost.png"),
               pygame.image.load("orangeGhost.png"), pygame.image.load("pinkGhost.png")]
-        
+
     def __init__(self, color):
         self.sprite = self.loadGhost(color)
         self.weak = False
         self.x_pos = 0
         self.y_pos = 0
+        self.current_level = level.Level('level2')
+        self.current_level_int = 1
+        self.point_map = copy.deepcopy(self.current_level.p_map)
+        self.disp = pygame.display.set_mode((1200, 600))
+        self.clock = pygame.time.Clock()
+        self.clock.tick(30)
+        self.current_direction = None
+        r = random.randint(1, 4)
+        if r == 1:
+            self.current_direction = "up"
+        elif r == 2:
+            self.current_direction = "down"
+        elif r == 3:
+            self.current_direction = "right"
+        else:
+            self.current_direction = "left"
+        loop = 0
 
     def loadGhost(self, color):
         temp = None
@@ -25,7 +48,6 @@ class Ghost():
             temp = self.images[3]
         return temp
 
-
     def notVulnerable(self):
         self.weak = False
 
@@ -37,14 +59,13 @@ class Ghost():
     # 30 px of move up and down -> 110px
     # spawn box at center of screen
 
-
     # after being eaten: middle -> left -> right
 
     def spawnOutside(self):
         # Designated for red ghost starting position
         # Only called when new level
         self.x_pos = 600
-        self.y_pos = 300
+        self.y_pos = 250
 
     def spawnleft(self):
         self.x_pos = 550
@@ -58,24 +79,64 @@ class Ghost():
         self.x_pos = 650
         self.y_pos = 350
 
+    def maze(self, color):
+        if color == "red":
+            for i in range(10):
+                self.y_pos -= 5
+                self.disp.blit(self.sprite, (self.x_pos, self.y_pos))
+
+    def moveChase(self, x, y):
+        ver = x - self.x_pos
+        hor = y - self.y_pos
+        if hor > 0 and self.current_level.check_valid(self.x_pos, self.y_pos + 5):
+            self.y_pos += 5
+        elif hor < 0 and self.current_level.check_valid(self.x_pos, self.y_pos - 5):
+            self.y_pos -= 5
+        elif ver > 0 and self.current_level.check_valid(self.x_pos + 5, self.y_pos):
+            self.x_pos += 5
+        elif ver < 0 and self.current_level.check_valid(self.x_pos - 5, self.y_pos):
+            self.x_pos -= 5
+
+    def moveRandom(self):
+        print(self.current_direction)
+        if self.current_direction == "up" and self.current_level.check_valid(self.x_pos, self.y_pos - 5):
+            self.y_pos -= 5
+        elif self.current_direction == "down" and self.current_level.check_valid(self.x_pos, self.y_pos + 5):
+            self.y_pos += 5
+        elif self.current_direction == "right" and self.current_level.check_valid(self.x_pos + 5, self.y_pos):
+            self.x_pos += 5
+        elif self.current_direction == "left" and self.current_level.check_valid(self.x_pos - 5, self.y_pos):
+            self.x_pos -= 5
+
+        else:
+            r = random.randint(1, 4)
+            # test if up is good
+            if r == 1 and self.current_level.check_valid(self.x_pos, self.y_pos - 5):
+                self.current_direction = "up"
+                self.y_pos -= 5
+            elif r == 2 and self.current_level.check_valid(self.x_pos, self.y_pos + 5):
+                self.current_direction = "down"
+                self.y_pos += 5
+            elif r == 3 and self.current_level.check_valid(self.x_pos + 5, self.y_pos):
+                self.current_direction = "right"
+                self.x_pos += 5
+            elif r == 4 and self.current_level.check_valid(self.x_pos - 5, self.y_pos):
+                self.current_direction = "left"
+                self.x_pos -= 5
 
     # TODO: movement algs
     # movement on new/ fresh level:
-        # red ghost spawns outside, right above the box
-        # up/down in box before spawn. after 5 seconds have passed, spawn new ghost
-        # 3 "slots" total in the box. (if somehow the player gets 4 ghost, make two overlap/ put in same slot)
-
-
+    # red ghost spawns outside, right above the box
+    # up/down in box before spawn. after 5 seconds have passed, spawn new ghost
+    # 3 "slots" total in the box. (if somehow the player gets 4 ghost, make two overlap/ put in same slot)
 
     # chase, scatter, frigtened
     # ghost only move 1 step ahead in map
-        # chase player for 20 secs
-            # try to chase pac man
-            # get position of pac man
+    # chase player for 20 secs
+    # try to chase pac man
+    # get position of pac man
 
+    # scatter for 7 secs
+    # go to corners of board
 
-        # scatter for 7 secs
-            # go to corners of board
-
-
-        # fright mode random generated
+    # fright mode random generated
