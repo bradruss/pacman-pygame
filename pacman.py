ce980eacdb3bbe7ff0e56ad56792acb2f959e94f
Bradley Russavage <brussava@uvm.edu>
2020-11-27 16:18:54 -0500

    added settings

--- pacman.py
+++ pacman.py
@@ -5,13 +5,14 @@ class Pacman:
         self.numLives = 5
         self.numCoins = 0
         self.isDangerous = False
+        self.sprite = [pg.image.load('pacman/Pacman.png'), pg.image.load('pacman/Pacman2.png'), pg.image.load('pacman/Pacman3.png')]
         if icon == 'biden':
             self.sprite = [pg.image.load('biden/close.png'), pg.image.load('biden/close2.png'), pg.image.load('biden/open.png')]
 
         # TODO: change the icon paths
         elif icon == 'trump':
             self.sprite = [pg.image.load('pacman/Pacman.png'), pg.image.load('pacman/Pacman2.png'), pg.image.load('pacman/Pacman3.png')]
-        else:
+        elif icon == 'pacman':
             self.sprite = [pg.image.load('pacman/Pacman.png'), pg.image.load('pacman/Pacman2.png'), pg.image.load('pacman/Pacman3.png')]
 
 
