9af8ff6b17a0dfd035a1011d6fbf3442bcd152bb
brandonleetheasian <bjlee@uvm.edu>
2020-11-29 16:28:45 -0500

    rolled back some more changes

--- levelCreator.py
+++ levelCreator.py
@@ -218,7 +218,84 @@ def main():
     # # both threads completely executed
     # print("Done!")
 
-    display_map()
+    global finished
+    pg.init()
+    disp = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    # Set window title
+    pg.display.set_caption("Pac-man")
+
+    # Load background
+    background = pg.image.load('background.jpg')
+
+    # Display the background image
+    disp.blit(background, (0, 0))
+
+    # Manage how fast the screen updates
+    clock = pg.time.Clock()
+
+    while not finished:
+        # makes it so user can exit out of window and end session
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                finished = True
+        # creates a black background
+        disp.fill((0, 0, 0))
+        # draws all currently created corridors
+        for key in curr_map:
+            curr_map[key].draw(disp)
+        pg.display.flip()
+        # 20 frames per second
+        clock.tick(20)
+        # prompts user for input
+        print(
+            "Input (name,H or V,start x,end x,start y,end y), or (join,horizontal corridor,vertical corridor,type), (del,corridor), (done,file_name), (revert, now), or (read_file, filename)")
+        val = input(": ")
+        val = val.replace('(', '')
+        val = val.replace(')', '')
+        val = val.replace(' ', '')
+        a = val.split(',')
+        print(a)
+        try:
+            # Makes a horizontal Hallway and stores it in the map
+            if a[1] == 'H':
+                curr_map[a[0]] = ch.CorridorH(int(a[2]), int(a[3]), int(a[4]))
+                copy_map = copy.deepcopy(curr_map)
+                iteration_log.append(copy_map)
+            # Makes a vertical hallway and stores it in the map
+            elif a[1] == 'V':
+                curr_map[a[0]] = cv.CorridorV(int(a[2]), int(a[4]), int(a[5]))
+                copy_map = copy.deepcopy(curr_map)
+                iteration_log.append(copy_map)
+            # Joins two speicfied hallways in the specified way
+            elif a[0] == 'join':
+                join(a[1], a[2], a[3])
+                copy_map = copy.deepcopy(curr_map)
+                iteration_log.append(copy_map)
+            # Deletes a specified hallway
+            elif a[0] == 'delete':
+                del curr_map[a[1]]
+                copy_map = copy.deepcopy(curr_map)
+                iteration_log.append(copy_map)
+            # Ends the creation process and stores the level map in a specified file
+            elif a[0] == 'done':
+                write_out(a[1])
+                finished = True
+            # Reverts level map back one iteration
+            elif a[0] == 'revert':
+                print("I'm about to revert")
+                curr_map.clear()
+                temp = copy.deepcopy(iteration_log[len(iteration_log) - 2])
+                curr_map.update(temp)
+                iteration_log.remove(iteration_log[len(iteration_log) - 1])
+            elif a[0] == 'read_file':
+                print("I'm about to read a file")
+                file_reader(a[1])
+                print("I'm done reading the file")
+            else:
+                print("incorrect input")
+        except:
+            print("exception error incorrect input")
 
 
 if __name__ == '__main__':
